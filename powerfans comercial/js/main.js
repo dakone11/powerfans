5dde2dd2c7c8fb10c13d88ad8794585523c68032
dakone11 <dakone11@gmail.com>
2022-03-29 18:28:09 +0200

    scripts por separado

--- powerfans comercial/js/main.js
+++ powerfans comercial/js/main.js
@@ -1,3 +1,5 @@
+
+//menu
 const $hamBtn = document.querySelector('.ham-btn'),
         $nav = document.querySelector('.nav');
 
@@ -76,53 +78,6 @@ window.onclick = function(event) {
 
 
 
-// animacion scroll
-
-/* Al cargarse la páfina añadimos un listener 
-/* al objeto window para obtener información 
-/* cada vez que se hace scroll */
-window.onload = function () {
-  window.addEventListener("scroll", onScroll);
-};
-
-/* Comprobamos si el div está dentro de la vista */
-function isCrolledIntoView(htmlElement) {
-  const rect = htmlElement.getBoundingClientRect();
-  const elementTop = rect.top;
-  const elementBottom = rect.bottom;
-  const isVisible = elementTop >= 0 && elementBottom <= window.innerHeight;
-  return isVisible;
-}
-
-function onScroll() {
-  // obtenemos todos los elementos html (divs) que tienen como id image
-  const images = document.querySelectorAll('[id="image"]');
-  // si no existen hacemos un return para que no ejecute el bucle
-  if (!images || images?.length === 0) return;
-  images.forEach((imageElement) => {
-    const isVisible = isCrolledIntoView(imageElement);
-    if (isVisible) {
-      // Si el div es visible añadimos al elemento img la clase de animación y eliminamos su propiedad id para que no se vuelva a realizar la animación.
-      addAnimationClass(imageElement);
-      removeId(imageElement);
-    }
-  });
-}
-
-function addAnimationClass(htmlElement) {
-  const nameClass = htmlElement.className;
-  // si la clase del elemento img tiene la clase left, añadimos la clase animated-left sino añadimos la clase animated-right
-  if (nameClass.includes("left")) {
-    htmlElement.classList.add("animated-left");
-  } else {
-    htmlElement.classList.add("animated-right");
-  }
-}
-
-/* Eliminamos el atributo id del elemento img */
-function removeId(htmlElement) {
-  htmlElement.removeAttribute("id");
-}
 
 
 // acordeon//
@@ -148,35 +103,7 @@ function toggleAccordion(element) {
 }
 
 
-//contador//
 
-// Date and Time for the countdown to end
-const launchTime = new Date("October 16, 2022 00:00:00").getTime();
-
-function countDown() {
-    const currentTime = new Date().getTime();
-    const timeDifference = launchTime - currentTime;
-
-    let seconds = Math.floor(timeDifference / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    let days = Math.floor(hours / 24);
-
-    hours = hours % 24;
-    minutes = minutes % 60;
-    seconds = seconds % 60;
-
-    hours = (hours < 10 && hours > 0) ? "0" + hours : hours;
-    minutes = (minutes < 10 && minutes > 0) ? "0" + minutes : minutes;
-    seconds = (seconds < 10 && seconds > 0) ? "0" + seconds : seconds;
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
-    document.getElementById("seconds").innerHTML = seconds;
-
-    setTimeout(countDown, 1000);
-}
-countDown();
 
 
 
