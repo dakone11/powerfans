e3c2d84b25648bd84b143d17dcf6d1f59baab913
dakone11 <dakone11@gmail.com>
2022-04-01 12:16:52 +0200

    cambios funcionamiento testimonial

--- powerfans comercial/js/testimonials.js
+++ powerfans comercial/js/testimonials.js
@@ -2,14 +2,12 @@ let slider = document.querySelector ('.testimonial');
 let sliderIndividual = document.querySelectorAll('.slider-testimonial');
 let contador=1;
 let tamañoWidth = sliderIndividual[0].clientWidth;
-let intervalo = 1000;
-
+let intervalo = 3000;
 setInterval(function tiempo(){
     slides();
 },intervalo);
-
 function slides () {
-    slider.style.transform = 'translate ('+ (- tamañoWidth * contador) + 'px)';
+    slider.style.transform = 'translate('+ (- tamañoWidth * contador) + 'px)';
     slider.style.transition = 'transform 1s';
     contador++;
     if (contador === sliderIndividual.length) {
@@ -17,8 +15,6 @@ function slides () {
 setTimeout(function() {
     slider.style.transform = 'translate (0px)';
     slider.style.transition = 'translate 0s)';
-
 }, intervalo)
     }
-
-} 
+}
