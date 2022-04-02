d76cd278dc3903701549ba1ef7c2d610540470e8
dakone11 <dakone11@gmail.com>
2022-04-03 01:09:01 +0200

    CAMBIO TEXTOS Y BORDER BOTTON

--- powerfans comercial/js/main.js
+++ powerfans comercial/js/main.js
@@ -64,11 +64,6 @@ btn.onclick = function() {
   popup.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  popup.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target === popup) {
